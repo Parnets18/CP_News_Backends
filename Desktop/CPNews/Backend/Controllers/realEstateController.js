6c026da4802d803f8fa30f9a752cd48efca28739
Parnets18 <parnetstech18@gmail.com>
2025-06-03 15:19:21 +0530

    Update realEstateController.js

--- Desktop/CPNews/Backend/Controllers/realEstateController.js
+++ Desktop/CPNews/Backend/Controllers/realEstateController.js
@@ -3,7 +3,8 @@ const RealEstate = require('../Models/realEstateModel');
 exports.createRealEstate = async (req, res) => {
   try {
     const { title, description, tags, date } = req.body;
-    const image = req.file ? req.file.path : '';
+    const image = req.file ? req.file.path.replace(/\\/g, '/') : '';
+
 
     const newProperty = await RealEstate.create({
       title,
@@ -23,7 +24,7 @@ exports.updateRealEstate = async (req, res) => {
   try {
     const { id } = req.params;
     const { title, description, tags, date } = req.body;
-    const image = req.file ? req.file.path : undefined;
+    const image = req.file ? req.file.path.replace(/\\/g, '/') : undefined;
 
     const updatedProperty = await RealEstate.findByIdAndUpdate(
       id,
