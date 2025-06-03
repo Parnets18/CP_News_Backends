6721ec5070394496bc5b4f44c9d21d72723ed9be
Parnets18 <parnetstech18@gmail.com>
2025-06-03 13:14:38 +0530

    Update server.js

--- Desktop/CPNews/Backend/server.js
+++ Desktop/CPNews/Backend/server.js
@@ -64,7 +64,7 @@ mongoose.connect(process.env.MONGODB_URI)
 app.get('/', (req, res) => res.send('🚓  API is live!'));
 
 
-app.use((req, res) => res.status(404).json({ message: 'Route not found' }));
+// app.use((req, res) => res.status(404).json({ message: 'Route not found' }));
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ message: 'Internal Server Error', error: err.message });
